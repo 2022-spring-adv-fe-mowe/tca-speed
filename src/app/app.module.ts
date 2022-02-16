02b3fd8f39b2d0736294314489bea4868b61739d
Tia Petts <tpetts@madisoncollege.edu>
2022-02-16 16:36:22 -0600

    Got Bootstrap up and running
    hit a snafu in the css framework world vs angular 13 world.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { GameTimerComponent } from './game-timer/game-timer.component';
