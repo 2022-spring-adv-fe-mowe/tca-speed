b147174612ea30d5be86ad881d6480902245adf1
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-21 16:30:44 -0600

    Kind of hacked in and "working"...
    At least a modal  is showing up ! ! !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { GameTimerComponent } from './game-timer/game-timer.component';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,9 @@ import { GameTimerComponent } from './game-timer/game-timer.component';
     GameTimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgbModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
