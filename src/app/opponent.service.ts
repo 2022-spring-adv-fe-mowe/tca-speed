382f8cd4aab596a55aaf78379ee1e4249aa3a27f
Tia Petts <tpetts@madisoncollege.edu>
2022-02-23 12:18:26 -0600

    added a service...
    ng generate service opponent --skip-tests
    
    not sure if this is where I wanted to go with this or not...we shall see.

---
+++ src/app/opponent.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+interface OpponentFromWeb {
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpponentService {
+
+  constructor() { }
+}
