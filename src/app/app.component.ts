8bcd4169b572020f0677dff1a488e28f547a8654
Tia Petts <tpetts@madisoncollege.edu>
2022-02-26 22:05:31 -0600

    display opponents?
    starting to work out the code to display the new opponents entered in by the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
+interface DisplayOpponents {
+  opponentName: string;
+  newOpponent: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,5 +20,20 @@ export class AppComponent {
 
   showModal = () => {
     const modalRef = this.modalSvc.open(UserProfileComponent);
+  }
+
+  player2: DisplayOpponents[] = [];
+
+  addNewOpponent = () => {
+    
+    const newOpponent: DisplayOpponents = {
+      opponentName: 'New Opponent'
+      , newOpponent: true
+    };
+
+    this.player2 = [
+      ...this.player2
+      , newOpponent
+    ]
   }
 }
