38452660a6b4cbd47173ed71aeadc94618360593
Tia Petts <tpetts@madisoncollege.edu>
2022-03-05 17:40:06 -0600

    Moving & Rearranging
    Moving things around and rearranging the home screen of my app to reflect the user story I had sketched out originally.
    
    Also added my favorite superhero to represent my companion app!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { appRoutingModule } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { GameTimerComponent } from './game-timer/game-timer.component';
+import { UserProfileComponent } from './user-profile';
+import { GameTimerComponent } from './game-timer';
 import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { GameSetupComponent } from './game-setup/game-setup.component';
+import { GameSetupComponent } from './game-setup';
+import { Router } from '@angular/router';
+import { HomeComponent } from './home';
+import { GameDataComponent } from './game-data/game-data.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserProfileComponent,
-    GameTimerComponent,
-    GameSetupComponent
+    AppComponent
+    , UserProfileComponent
+    , GameTimerComponent
+    , GameSetupComponent
+    , HomeComponent
+    , GameDataComponent
   ],
   imports: [
     BrowserModule
+    , appRoutingModule
     , NgbModalModule
     , NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
