80a315d37006aea628fdd112a0bd4857ed7f1af0
Tia Petts <tpetts@madisoncollege.edu>
2022-03-09 12:10:11 -0600

    Routing to Player List Component
    This is where the user will be able to add players to the application, hopefully, provided that the radio button imports don't break the app again.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { GameSetupComponent } from './game-setup';
 import { GameTimerComponent } from './game-timer';
 import { UserProfileComponent } from './user-profile';
 import { GameDataComponent } from './game-data';
+import { PlayerListComponent } from './player-list';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -12,7 +13,7 @@ const routes: Routes = [
     { path: 'game-timer', component: GameTimerComponent },
     { path: 'user-profile', component: UserProfileComponent },
     { path: 'game-data', component: GameDataComponent },
-
+    { path: 'player-list', component: PlayerListComponent },
     // otherwise redirect to home
     { path: '**', redirectTo: './home' }
 ];
