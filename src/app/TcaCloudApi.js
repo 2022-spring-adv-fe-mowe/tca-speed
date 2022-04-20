9edde522197c4234530effab3e578ba76beebce0
Tia Petts <tpetts@madisoncollege.edu>
2022-04-20 15:35:54 -0500

    temp fix for now
    silenced files and installed popper package

--- src/app/TcaCloudApi.js
+++ src/app/TcaCloudApi.js
@@ -1,62 +1,62 @@
-import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
-​
-export const saveGameToCloud = async (email, appName, timestamp, gameResult) => {
-​
-    const dynamoGame = {
-      pk: email
-      , sk: `${appName}#${timestamp}`
+// import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+// ​
+// export const saveGameToCloud = async (email, appName, timestamp, gameResult) => {
+// ​
+//     const dynamoGame = {
+//       pk: email
+//       , sk: `${appName}#${timestamp}`
   
-      , ts: timestamp
-      , user: email
-      , app: appName
+//       , ts: timestamp
+//       , user: email
+//       , app: appName
   
-      , gsi1pk: appName
-      , gsi1sk: timestamp
+//       , gsi1pk: appName
+//       , gsi1sk: timestamp
   
-      , game: gameResult
-    };
+//       , game: gameResult
+//     };
   
-    console.log("Unmarshalled Game", dynamoGame);
-​
-    const marshalledGame = marshall(
-      dynamoGame
-      , {
-        removeUndefinedValues: true
-        , convertClassInstanceToMap: true
-      }
-    );
+//     console.log("Unmarshalled Game", dynamoGame);
+// ​
+//     const marshalledGame = marshall(
+//       dynamoGame
+//       , {
+//         removeUndefinedValues: true
+//         , convertClassInstanceToMap: true
+//       }
+//     );
   
-    console.log("MarshalledGame", marshalledGame);
+//     console.log("MarshalledGame", marshalledGame);
   
-    const options = {
-      method: 'POST',
-      body: JSON.stringify({
-        TableName: "tca-data",
-        Item: marshalledGame
-      })  
-    };
+//     const options = {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         TableName: "tca-data",
+//         Item: marshalledGame
+//       })  
+//     };
   
-    await fetch(
-      "https://32wop75hhc.execute-api.us-east-1.amazonaws.com/prod/data"
-      , options 
-    );
-  };
+//     await fetch(
+//       "https://32wop75hhc.execute-api.us-east-1.amazonaws.com/prod/data"
+//       , options 
+//     );
+//   };
   
-  export const loadGamesFromCloud = async (email, appName) => {
+//   export const loadGamesFromCloud = async (email, appName) => {
       
-    const url = `https://32wop75hhc.execute-api.us-east-1.amazonaws.com/prod/data/?user=${email}&game=${appName}`;
+//     const url = `https://32wop75hhc.execute-api.us-east-1.amazonaws.com/prod/data/?user=${email}&game=${appName}`;
     
-    console.log("url", url);
+//     console.log("url", url);
     
-    const response = await fetch(url);
-    const data = await response.json();
+//     const response = await fetch(url);
+//     const data = await response.json();
     
-    console.log("Marshalled Response", data);
+//     console.log("Marshalled Response", data);
     
-    const unmarshalledData = data.Items.map(x => unmarshall(x, {convertEmptyValues: true}));
+//     const unmarshalledData = data.Items.map(x => unmarshall(x, {convertEmptyValues: true}));
     
-    console.log("Unarshalled Response", unmarshalledData);
-​
-    const gameResults = unmarshalledData.map(x => x.game);
-    return gameResults;    
-  };
+//     console.log("Unarshalled Response", unmarshalledData);
+// ​
+//     const gameResults = unmarshalledData.map(x => x.game);
+//     return gameResults;    
+//   };
